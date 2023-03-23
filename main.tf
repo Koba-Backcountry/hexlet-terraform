7d299f9acbe9b7ae10286cc0e6ed0459586c25ff
Koba Skhulukhia <ipm.it.manager@gmail.com>
2023-03-23 17:19:32 +0400

    modified main.tf

--- main.tf
+++ main.tf
@@ -1,41 +1,54 @@
 terraform {
   required_providers {
-    // Здесь указываются все провайдеры, которые будут использоваться
     digitalocean = {
       source = "digitalocean/digitalocean"
-      // Версия может обновиться
       version = "~> 2.0"
+    }
+    datadog = {
+      source = "DataDog/datadog"
     }
   }
 }
 
-// Terraform должен знать ключ, для выполнения команд по API
-
-// Определение переменной, которую нужно будет задать
 variable "do_token" {}
 
-// Установка значения переменной
+variable "datadog_api_key" {}
+
+variable "datadog_app_key" {}
+
+variable "datadog_api_url" {}
+
+variable "web_srv1" {}
+
+variable "web_srv2" {}
+
 provider "digitalocean" {
   token = var.do_token
 }
 
-// Пример взят из документации
-// web - произвольное имя ресурса
+provider "datadog" {
+  api_key = var.datadog_api_key
+  app_key = var.datadog_app_key
+  api_url = var.datadog_api_url
+}
+
 resource "digitalocean_droplet" "web1" {
   image  = "ubuntu-18-04-x64"
-  // Имя внутри Digital Ocean
-  // Задается для удобства просмотра в веб-интерфейсе
-  name   = "web-1"
-  // Регион, в котором располагается датацентр
-  // Выбирается по принципу близости к клиентам
+  name   = var.web_srv1
   region = "ams3"
-  // Тип сервера, от этого зависит его мощность и стоимость
   size   = "s-1vcpu-1gb"
 }
 
 resource "digitalocean_droplet" "web2" {
   image  = "ubuntu-18-04-x64"
-  name   = "web-2"
+  name   = var.web_srv2
   region = "ams3"
   size   = "s-1vcpu-1gb"
+}
+
+resource "datadog_monitor" "cpumonitor" {
+  name = "cpu monitor"
+  type = "metric alert"
+  message = "CPU usage alert"
+  query = "avg(last_1m):avg:system.cpu.system{*} by {host} > 60"
 }
